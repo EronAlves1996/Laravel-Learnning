ee6cf453b686fa4e52468f1bb25170d8a98f0666
Eron Alves <eron.alves@rocketmail.com>
2023-06-16 01:47:42 -0300

    feat: create a blade directive

--- resources/views/page/result.blade.php
+++ resources/views/page/result.blade.php
@@ -4,4 +4,9 @@
     @foreach ($filtered as $item)
         <p>{{ $item }}</p>
     @endforeach
+
+    <h3>Resultados em Base64</h3>
+    @foreach ($filtered as $item)
+        <p>@toBase64($item)</p>
+    @endforeach
 @endsection
